d15dd34e912bebde52204379d220d025bbc81b0b
Akash <akashwaris.cybersec@gmail.com>
2024-12-06 08:19:16 +0500

    edit color file

---
+++ color.h
@@ -0,0 +1,57 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+// Reset
+#define RESET "\033[0m"
+
+// Regular Colors
+#define BLACK "\033[30m"      // Black
+#define RED "\033[31m"        // Red
+#define GREEN "\033[32m"      // Green
+#define YELLOW "\033[33m"     // Yellow
+#define BLUE "\033[34m"       // Blue
+#define MAGENTA "\033[35m"    // Magenta
+#define CYAN "\033[36m"       // Cyan
+#define WHITE "\033[37m"      // White
+
+// Bold Colors
+#define BOLD_BLACK "\033[1;30m"  // Bold Black
+#define BOLD_RED "\033[1;31m"    // Bold Red
+#define BOLD_GREEN "\033[1;32m"  // Bold Green
+#define BOLD_YELLOW "\033[1;33m" // Bold Yellow
+#define BOLD_BLUE "\033[1;34m"   // Bold Blue
+#define BOLD_MAGENTA "\033[1;35m"// Bold Magenta
+#define BOLD_CYAN "\033[1;36m"   // Bold Cyan
+#define BOLD_WHITE "\033[1;37m"  // Bold White
+
+// Background Colors
+#define BG_BLACK "\033[40m"   // Black Background
+#define BG_RED "\033[41m"     // Red Background
+#define BG_GREEN "\033[42m"   // Green Background
+#define BG_YELLOW "\033[43m"  // Yellow Background
+#define BG_BLUE "\033[44m"    // Blue Background
+#define BG_MAGENTA "\033[45m" // Magenta Background
+#define BG_CYAN "\033[46m"    // Cyan Background
+#define BG_WHITE "\033[47m"   // White Background
+
+// High Intensity Colors
+#define HI_BLACK "\033[90m"   // High Intensity Black
+#define HI_RED "\033[91m"     // High Intensity Red
+#define HI_GREEN "\033[92m"   // High Intensity Green
+#define HI_YELLOW "\033[93m"  // High Intensity Yellow
+#define HI_BLUE "\033[94m"    // High Intensity Blue
+#define HI_MAGENTA "\033[95m" // High Intensity Magenta
+#define HI_CYAN "\033[96m"    // High Intensity Cyan
+#define HI_WHITE "\033[97m"   // High Intensity White
+
+// High Intensity Bold Colors
+#define BOLD_HI_BLACK "\033[1;90m"  // Bold High Intensity Black
+#define BOLD_HI_RED "\033[1;91m"    // Bold High Intensity Red
+#define BOLD_HI_GREEN "\033[1;92m"  // Bold High Intensity Green
+#define BOLD_HI_YELLOW "\033[1;93m" // Bold High Intensity Yellow
+#define BOLD_HI_BLUE "\033[1;94m"   // Bold High Intensity Blue
+#define BOLD_HI_MAGENTA "\033[1;95m"// Bold High Intensity Magenta
+#define BOLD_HI_CYAN "\033[1;96m"   // Bold High Intensity Cyan
+#define BOLD_HI_WHITE "\033[1;97m"  // Bold High Intensity White
+
+#endif // COLOR_H

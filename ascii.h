8baca18ac6bc4d3530f6fe7cc7b962b9541ec721
Akash <akashwaris.cybersec@gmail.com>
2024-12-07 11:12:27 +0500

    NEw files of sound

---
+++ ascii.h
@@ -0,0 +1,90 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+#include "color.h" // Include your color library
+#include <iostream>
+#include <windows.h>  // For Sleep and clearing the console
+using namespace std;
+
+
+// Function to play a sound at the beginning
+void playSound() {
+    // Replace the path with the location of your .wav file
+    PlaySound(TEXT("sound.wav"), NULL, SND_FILENAME | SND_ASYNC);
+}
+void playwelcomesound() {
+    // Replace the path with the location of your .wav file
+    PlaySound(TEXT("welcome.wav"), NULL, SND_FILENAME | SND_ASYNC);
+}
+// Function to print text character by character with a delay
+void printCharByChar(const string& text, const string& color = RESET) {
+    for (char ch : text) {
+        cout << color << ch << RESET;  // Apply color to each character
+        Sleep(50);  // Add a delay between characters for effect
+    }
+    cout << endl;
+    Sleep(500);  // Pause after finishing the text
+}
+
+// Function to display a welcome animation with the bridge and airplane
+void displayWelcome() {
+    // Bridge and airplane ASCII art
+    string art = R"(
+         @\_______/@
+        @|XXXXXXXX |
+       @ |X||    X |
+      @  |X||    X |
+     @   |XXXXXXXX |
+    @    |X||    X |             V
+   @     |X||   .X |                                               |
+  @      |X||.  .X |                      V                  --====|====--
+ @      |%XXXXXXXX%||                                              |
+@       |X||  . . X||                                          .-"""""-. 
+        |X||   .. X||                                        .'_________'. 
+        |X||  .   X||                                       /_/_|__|__|_\_\
+        |X|| .    X||                                      ;'-._       _.-';
+       |XXXXXXXXXXXX||                ,--------------------|    `-. .-'    |--------------------,
+       |XXXXXXXXXXXX||                 ``""--..__    ___   ;       '       ;   ___    __..--""```   
+       |XX|        X||                           `"-// \\.._\             /_..// \\-"`         
+       |XX|        X||                              \\_//    '._       _.'    \\_// 
+       |XX|        X||                               `"`        ``---``        `"` 
+       |XX|======= X||==========================+|==============================================+
+    )";
+
+    system("cls");
+    // Print the art in bold cyan for a soothing look
+    cout << BOLD_CYAN << art << RESET << endl;
+
+    // Tagline after the art
+    cout << string(BOLD_WHITE) + BG_BLUE + "======================================================================================================================================" + RESET << endl;
+    cout << BOLD_YELLOW << "\n\t\tFlight Path Visualizer - SkyNav AI" << RESET << endl;
+    cout << BOLD_GREEN << "\t\t\t\t\t\t\t\tWhere every journey is a masterpiece of connectivity.\n" << RESET << endl;
+    cout << BOLD_RED << "\tM.AKASH WARIS     HABIB AHMED    ABDULLAH NOMAN" << RESET << endl;
+    cout << string(BOLD_WHITE) + BG_BLUE + "======================================================================================================================================" + RESET << endl;
+    Sleep(2000); // Pause for 2 seconds to let the user absorb the welcome screen
+}
+
+// Function to display the ASCII art title with a delay effect
+void printAsciiArtWithDelay() {
+    string asciiArt = R"(
+ 
+
+ _      _____ _     ____  ____  _      _____   _____  ____    ____  _  _____  _ _      ____  _       ____  _ 
+/ \  /|/  __// \   /   _\/  _ \/ \__/|/  __/  /__ __\/  _ \  / ___\/ |/ /\  \/// \  /|/  _ \/ \ |\  /  _ \/ \
+| |  |||  \  | |   |  /  | / \|| |\/|||  \      / \  | / \|  |    \|   /  \  / | |\ ||| / \|| | //  | / \|| |
+| |/\|||  /_ | |_/\|  \_ | \_/|| |  |||  /_     | |  | \_/|  \___ ||   \  / /  | | \||| |-||| \//   | |-||| |
+\_/  \|\____\\____/\____/\____/\_/  \|\____\    \_/  \____/  \____/\_|\_\/_/   \_/  \|\_/ \|\__/    \_/ \|\_/
+                                                                                                             
+
+                                                                                                             
+)";
+    // Print the title in bold yellow
+    for (char ch : asciiArt) {
+        cout << BOLD_YELLOW  <<ch << RESET;
+        Sleep(10); // Add delay between characters
+    }
+    Sleep(500); // Final delay after printing the ASCII art
+}
+
+
+#endif
